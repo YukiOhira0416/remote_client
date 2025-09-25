2e99a66946fddfffa98d872aea4526af732cc32a
Your Name <you@example.com>
2025-09-25 16:01:40 +0900

    軽微な修正

--- Globals.cpp
+++ Globals.cpp
@@ -33,7 +33,7 @@ std::atomic<bool> app_running_atomic(true);
 std::atomic<bool> g_isSizing{false};
 std::atomic<bool> g_showRebootOverlay{ false };
 std::atomic<bool> g_forcePresentOnce{false};
-std::atomic<bool> dumpEncodedStreamToFiles{false};
+std::atomic<bool> dumpEncodedStreamToFiles{true};
 
 // Encoded Frame queue
 moodycamel::ConcurrentQueue<EncodedFrame> g_encodedFrameQueue;
