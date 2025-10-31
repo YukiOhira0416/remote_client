30260495dd1b348c31748603652ab433d4a3d4f0
Your Name <you@example.com>
2025-10-31 19:40:12 +0900

    軽微な修正

--- window.cpp
+++ window.cpp
@@ -2074,7 +2074,7 @@ void RenderFrame() {
                 auto frameEndSys = std::chrono::system_clock::now();
                 uint64_t frameEndMs =
                     std::chrono::duration_cast<std::chrono::milliseconds>(frameEndSys.time_since_epoch()).count();
-                wgc_to_renderend_ms = static_cast<int64_t>(frameEndMs) - static_cast<int64_t>(wgc_ts_ms) + g_TimeOffsetNs;
+                wgc_to_renderend_ms = static_cast<int64_t>(frameEndMs) - static_cast<int64_t>(wgc_ts_ms) + (g_TimeOffsetNs.load() / 1000000);
             }
         }
 
