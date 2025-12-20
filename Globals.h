9896357d8dd7b42a56b2ba44564ef22cf229a89a
YukiOhira0416 <xylish.hyper.cool@icloud.com>
2025-12-21 04:16:05 +0900

    Prevent TIMECODE redefinition error

--- Globals.h
+++ Globals.h
@@ -1,5 +1,30 @@
 #ifndef GLOBALS_H
 #define GLOBALS_H
+// Windows SDK 10.0.26100 以降では ksmedia.h が TIMECODE を再定義する場合があり、
+// Visual Studio ビルド時に C2371 が発生することがある。
+// 事前に __TIMECODE_DEFINED を定義して再定義を抑止する。
+#ifndef __TIMECODE_DEFINED
+#define __TIMECODE_DEFINED
+// strmif.h と同じ定義を明示しておく。
+typedef struct tagTIMECODE {
+    WORD  wFrameRate;
+    WORD  wFrameFract;
+    DWORD dwFrames;
+    DWORD dwSeconds;
+    DWORD dwMinutes;
+    DWORD dwHours;
+    DWORD dwFlags;
+} TIMECODE;
+
+typedef struct tagTIMECODE_SAMPLE {
+    TIMECODE timecode;
+    DWORD    dwUser;
+    DWORD    dwFlags;
+    DWORD    dwReserved1;
+    DWORD    dwReserved2;
+} TIMECODE_SAMPLE;
+#endif // __TIMECODE_DEFINED
+
 #include <windows.h>
 #include <atomic>
 #include <string>
