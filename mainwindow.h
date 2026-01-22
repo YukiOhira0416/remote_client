79061c9e54b83c59ae4f61d50191ea90d2f70d87
Your Name <you@example.com>
2026-01-23 00:21:12 +0900

    メインウインドウの修正

--- mainwindow.h
+++ mainwindow.h
@@ -11,9 +11,14 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QComboBox>
+#include <QtWidgets/QGroupBox>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
+#include <QtWidgets/QTabWidget>
 #include <QtWidgets/QWidget>
 #include "renderhostwidgets.h"
 
@@ -23,7 +28,17 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QTabWidget *tabWidget;
+    QWidget *tab;
     RenderHostWidgets *frame;
+    QWidget *tab_2;
+    QComboBox *comboBox;
+    QLabel *label;
+    QGroupBox *groupBox;
+    QRadioButton *radioButton;
+    QRadioButton *radioButton_2;
+    QRadioButton *radioButton_3;
+    QRadioButton *radioButton_4;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -31,23 +46,61 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(1568, 806);
-        centralwidget = new QWidget(MainWindow);
-        centralwidget->setObjectName("centralwidget");
-        frame = new RenderHostWidgets(centralwidget);
-        frame->setObjectName("frame");
-        frame->setGeometry(QRect(20, 20, 1280, 720));
+        MainWindow->resize(1502, 845);
         QSizePolicy sizePolicy(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Expanding);
         sizePolicy.setHorizontalStretch(0);
         sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(MainWindow->sizePolicy().hasHeightForWidth());
+        MainWindow->setSizePolicy(sizePolicy);
+        MainWindow->setMinimumSize(QSize(1502, 845));
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName("centralwidget");
+        tabWidget = new QTabWidget(centralwidget);
+        tabWidget->setObjectName("tabWidget");
+        tabWidget->setGeometry(QRect(10, 10, 1311, 771));
+        tab = new QWidget();
+        tab->setObjectName("tab");
+        frame = new RenderHostWidgets(tab);
+        frame->setObjectName("frame");
+        frame->setGeometry(QRect(20, 10, 1280, 720));
         sizePolicy.setHeightForWidth(frame->sizePolicy().hasHeightForWidth());
         frame->setSizePolicy(sizePolicy);
         frame->setFrameShape(QFrame::Shape::StyledPanel);
         frame->setFrameShadow(QFrame::Shadow::Raised);
+        tabWidget->addTab(tab, QString());
+        tab_2 = new QWidget();
+        tab_2->setObjectName("tab_2");
+        tabWidget->addTab(tab_2, QString());
+        comboBox = new QComboBox(centralwidget);
+        comboBox->setObjectName("comboBox");
+        comboBox->setGeometry(QRect(1350, 750, 141, 31));
+        QSizePolicy sizePolicy1(QSizePolicy::Policy::Expanding, QSizePolicy::Policy::Fixed);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(comboBox->sizePolicy().hasHeightForWidth());
+        comboBox->setSizePolicy(sizePolicy1);
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(1350, 720, 101, 31));
+        groupBox = new QGroupBox(centralwidget);
+        groupBox->setObjectName("groupBox");
+        groupBox->setGeometry(QRect(1340, 30, 161, 161));
+        radioButton = new QRadioButton(groupBox);
+        radioButton->setObjectName("radioButton");
+        radioButton->setGeometry(QRect(10, 36, 92, 20));
+        radioButton_2 = new QRadioButton(groupBox);
+        radioButton_2->setObjectName("radioButton_2");
+        radioButton_2->setGeometry(QRect(10, 64, 92, 20));
+        radioButton_3 = new QRadioButton(groupBox);
+        radioButton_3->setObjectName("radioButton_3");
+        radioButton_3->setGeometry(QRect(10, 92, 92, 20));
+        radioButton_4 = new QRadioButton(groupBox);
+        radioButton_4->setObjectName("radioButton_4");
+        radioButton_4->setGeometry(QRect(10, 120, 92, 20));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 1568, 33));
+        menubar->setGeometry(QRect(0, 0, 1502, 33));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -61,6 +114,14 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab), QCoreApplication::translate("MainWindow", "Tab 1", nullptr));
+        tabWidget->setTabText(tabWidget->indexOf(tab_2), QCoreApplication::translate("MainWindow", "Tab 2", nullptr));
+        label->setText(QCoreApplication::translate("MainWindow", "Select Keyboard", nullptr));
+        groupBox->setTitle(QCoreApplication::translate("MainWindow", "Select Display", nullptr));
+        radioButton->setText(QCoreApplication::translate("MainWindow", "Display 1", nullptr));
+        radioButton_2->setText(QCoreApplication::translate("MainWindow", "Display 2", nullptr));
+        radioButton_3->setText(QCoreApplication::translate("MainWindow", "Display 3", nullptr));
+        radioButton_4->setText(QCoreApplication::translate("MainWindow", "Display 4", nullptr));
     } // retranslateUi
 
 };
