89ca809a55df444bb507bac690c1b109316ffdd6
YukiOhira0416 <your-github-email@example.com>
2026-02-15 08:13:50 +0900

    特殊キー操作UIを追加

--- mainwindow.h
+++ mainwindow.h
@@ -14,9 +14,11 @@
 #include <QtWidgets/QGroupBox>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QRadioButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QTabWidget>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 #include "renderhostwidgets.h"
 
@@ -35,6 +37,14 @@ public:
     QRadioButton *radioButton_2;
     QRadioButton *radioButton_3;
     QRadioButton *radioButton_4;
+    QGroupBox *groupBox_2;
+    QWidget *verticalLayoutWidget;
+    QVBoxLayout *verticalLayout;
+    QRadioButton *radioButton_5;
+    QRadioButton *radioButton_6;
+    QRadioButton *radioButton_7;
+    QRadioButton *radioButton_9;
+    QPushButton *pushButton;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -82,6 +92,40 @@ public:
         radioButton_4 = new QRadioButton(groupBox);
         radioButton_4->setObjectName("radioButton_4");
         radioButton_4->setGeometry(QRect(10, 120, 92, 20));
+        groupBox_2 = new QGroupBox(centralwidget);
+        groupBox_2->setObjectName("groupBox_2");
+        groupBox_2->setGeometry(QRect(1340, 210, 151, 231));
+        verticalLayoutWidget = new QWidget(groupBox_2);
+        verticalLayoutWidget->setObjectName("verticalLayoutWidget");
+        verticalLayoutWidget->setGeometry(QRect(10, 20, 131, 201));
+        verticalLayout = new QVBoxLayout(verticalLayoutWidget);
+        verticalLayout->setObjectName("verticalLayout");
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        radioButton_5 = new QRadioButton(verticalLayoutWidget);
+        radioButton_5->setObjectName("radioButton_5");
+
+        verticalLayout->addWidget(radioButton_5);
+
+        radioButton_6 = new QRadioButton(verticalLayoutWidget);
+        radioButton_6->setObjectName("radioButton_6");
+
+        verticalLayout->addWidget(radioButton_6);
+
+        radioButton_7 = new QRadioButton(verticalLayoutWidget);
+        radioButton_7->setObjectName("radioButton_7");
+
+        verticalLayout->addWidget(radioButton_7);
+
+        radioButton_9 = new QRadioButton(verticalLayoutWidget);
+        radioButton_9->setObjectName("radioButton_9");
+
+        verticalLayout->addWidget(radioButton_9);
+
+        pushButton = new QPushButton(verticalLayoutWidget);
+        pushButton->setObjectName("pushButton");
+
+        verticalLayout->addWidget(pushButton);
+
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
@@ -106,6 +150,12 @@ public:
         radioButton_2->setText(QCoreApplication::translate("MainWindow", "Display 2", nullptr));
         radioButton_3->setText(QCoreApplication::translate("MainWindow", "Display 3", nullptr));
         radioButton_4->setText(QCoreApplication::translate("MainWindow", "Display 4", nullptr));
+        groupBox_2->setTitle(QCoreApplication::translate("MainWindow", "Shortcut Key", nullptr));
+        radioButton_5->setText(QCoreApplication::translate("MainWindow", "Win + L", nullptr));
+        radioButton_6->setText(QCoreApplication::translate("MainWindow", "Win + G", nullptr));
+        radioButton_7->setText(QCoreApplication::translate("MainWindow", "Win + Alt + B", nullptr));
+        radioButton_9->setText(QCoreApplication::translate("MainWindow", "Ctrl + Alt + Delete", nullptr));
+        pushButton->setText(QCoreApplication::translate("MainWindow", "Key Send", nullptr));
     } // retranslateUi
 
 };
